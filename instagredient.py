48d40794545847181195088827ff7051fbe06aca
J Oslislo <slothingtonspam@gmail.com>
2017-11-02 14:17:36 -0700

    Config, web stubs

--- instagredient.py
+++ instagredient.py
@@ -3,4 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-        return "Hello World!"
+    return "Hello World!"
+
+@app.route("/callback")
+def callback():
+    return "Callback"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return "do_the_login"
+    else:
+        return "show_the_login_form"
