c066d458ed9ef1059b090f824182f20d599b94c4
J Oslislo <slothingtonspam@gmail.com>
2017-11-02 16:46:27 -0700

    logging

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 from web import app
+import logging
+from logging.handlers import RotatingFileHandler
 
 if __name__ == "__main__":
+    handler = RotatingFileHandler('instagredient.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
     app.run()
