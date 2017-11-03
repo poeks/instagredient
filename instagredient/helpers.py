580b5da8c0d0e67ac56993f367fc6142fab03dbf
J Oslislo <slothingtonspam@gmail.com>
2017-11-02 20:14:00 -0700

    session parsing

--- instagredient/helpers.py
+++ instagredient/helpers.py
@@ -1,7 +1,30 @@
-import ConfigParser
+import ConfigParser, requests
+from flask import redirect
 
 def read_config(config_file="./config.cfg"):
     config = ConfigParser.ConfigParser()
     config.read(config_file)
 
     return config
+
+def token(session, from_url):
+    name = 'access_token'
+    if session.get(name):
+        return session.get(name)
+    else:
+        return redirect('/callback?redirect_url={}'.format(from_url))
+
+def exchange_code(request, session):
+
+    if request.args.get('code'):
+        access_url = CONFIG.get('wunderlist', 'auth_access_url')
+        code = request.args.get('code')
+        client_secret = CONFIG.get('wunderlist', 'client_secret')
+        r = requests.post(access_url, data={'code':code, 'client_id':client_id, 'client_secret':client_secret})
+        json_res = r.json()
+
+        if 'access_token' in json_res:
+            session['access_token'] = json_res['access_token']
+        else:
+            return "error:{}".format(r.json())
+
--- web.py
+++ web.py
@@ -2,11 +2,11 @@ import requests
 from flask import Flask, request, redirect, session
 from flask_session import Session
 from env import CONFIG
+from instagredient import helpers
 
 
 app = Flask(__name__)
 app.secret_key = "yadda yadda wooooo"
-#app.config.from_object(__name__)
 sess = Session()
 
 @app.route("/")
@@ -16,38 +16,53 @@ def hello():
     else:
         return "Hello World!"
 
+@app.route("/test")
+def test():
+    if request.args.get('code'):
+        helpers.exchange_code(request, session)
+
+    token = helpers.token(session, 'test')
+    return "token is {}".format(token)
 
 @app.route("/callback")
 def callback():
     client_id = CONFIG.get('wunderlist', 'client_id')
 
     if request.args.get('code'):
-        access_url = CONFIG.get('wunderlist', 'auth_access_url')
-        code = request.args.get('code')
-        client_secret = CONFIG.get('wunderlist', 'client_secret')
-        r = requests.post(access_url, data={'code':code, 'client_id':client_id, 'client_secret':client_secret})
-        json_res = r.json()
-
-        if 'access_token' in json_res:
-            session['access_token'] = json_res['access_token']
-        else:
-            return "error:{}".format(r.json())
+        helpers.exchange_code(request, session)
+        # access_url = CONFIG.get('wunderlist', 'auth_access_url')
+        # code = request.args.get('code')
+        # client_secret = CONFIG.get('wunderlist', 'client_secret')
+        # r = requests.post(access_url, data={'code':code, 'client_id':client_id, 'client_secret':client_secret})
+        # json_res = r.json()
+        #
+        # if 'access_token' in json_res:
+        #     session['access_token'] = json_res['access_token']
+        # else:
+        #     return "error:{}".format(r.json())
 
         return redirect("/")
 
     else:
         client_id = CONFIG.get('wunderlist', 'client_id')
-        redirect_url = CONFIG.get('wunderlist', 'app_callback_url')
+        #redirect_url = CONFIG.get('wunderlist', 'app_callback_url')
+        redirect_url = "{}/{}".format(CONFIG.get('wunderlist', 'app_url'), request.args.get('redirect_url'))
         state = CONFIG.get('wunderlist', 'state')
         auth_url = CONFIG.get('wunderlist', 'auth_url')
         url = "{}?client_id={}&redirect_uri={}&state={}".format(auth_url, client_id, redirect_url, state)
 
         return redirect(url)
 
+@app.route("/add")
+def add():
+    # check if list exists
+    # parse url
+    # add items to list
+    return "test"
+
 
 if __name__ == "__main__":
     app.config['SESSION_TYPE'] = 'filesystem'
 
     sess.init_app(app)
-    #app.debug = True
     app.run('127.0.0.1')
